f36ddc6471731c959dfaeeabe6fa6cce962e9ad5
Matt <mqunell@pm.me>
2023-04-21 19:35:34 -0700

    spaghetti

--- node-client/client.ts
+++ node-client/client.ts
@@ -1,1 +1,23 @@
 import grpc from '@grpc/grpc-js';
+import protoLoader from '@grpc/proto-loader';
+
+const PROTO_PATH = './proto/tutorial.proto';
+
+const grpcObj = protoLoader.loadSync(PROTO_PATH, {
+	keepCase: true,
+	longs: String,
+	enums: String,
+	defaults: true,
+	oneofs: true,
+});
+
+const TutorialService = grpc.loadPackageDefinition(grpcObj).TutorialService;
+
+/* const client = new TutorialService(
+	'localhost:9001',
+	grpc.credentials.createInsecure()
+);
+
+client.sayHello({}, (error, res) => {
+	console.log(res);
+}); */
